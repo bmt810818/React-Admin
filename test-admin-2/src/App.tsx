3590bf412152b716e873d0a6454e27c29425703e
Bui Minh Toan <toan.bui@nexpando.com>
2024-06-28 20:41:31 +0700

    Refactor

--- test-admin-2/src/App.tsx
+++ test-admin-2/src/App.tsx
@@ -4,7 +4,13 @@ import {
 } from "react-admin";
 import { Layout } from "./Layout";
 import UserList from "./components/users/List";
-import dataProvider from "./dataProvider";
+// import dataProvider from "./dataProvider";
+import simpleRestProvider from "ra-data-simple-rest";
+import jsonServerProvider from 'ra-data-json-server';
+
+
+
+const dataProvider = jsonServerProvider('http://localhost:8080');
 
 
 export const App = () => (
