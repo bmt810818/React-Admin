a8a3b3d7a15095a1b286fdc24ab3c31f7e8e87f4
Bui Minh Toan <toan.bui@nexpando.com>
2024-07-06 21:31:09 +0700

    Add genetic create

--- test-admin-2/src/components/genetics/GeneticCreate.tsx
+++ test-admin-2/src/components/genetics/GeneticCreate.tsx
@@ -1,15 +1,13 @@
-import { Create, DateInput, required, SimpleForm, TextInput } from "react-admin"
+import {Create, ImageField, ImageInput, required, SimpleForm, TextInput} from "react-admin"
 
 const GeneticCreate = () => (
   <Create>
     <SimpleForm>
-            <TextInput source="name" validate={[required()]} />
-            <TextInput source="image" validate={[required()]} />
-            <TextInput source="dna_code" validate={[required()]} />
-            <TextInput source="rna_code" validate={[required()]} />
-            <TextInput source="status" validate={[required()]} />
-            <TextInput source="describe" validate={[required()]} />
-        </SimpleForm>
+        <TextInput source="name" validate={[required()]} />
+        <TextInput source="code" validate={[required()]} />
+        <TextInput source="status" validate={[required()]} />
+        <TextInput source="description" validate={[required()]} />
+    </SimpleForm>
   </Create>
 )
 
