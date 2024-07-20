cca13bbae5747743c0176a2ea75ee95f1a6c961b
Bui Minh Toan <toan.bui@nexpando.com>
2024-07-20 11:19:39 +0700

    Update GeneticCreate

--- test-admin-2/src/components/genetics/GeneticCreate.tsx
+++ test-admin-2/src/components/genetics/GeneticCreate.tsx
@@ -1,14 +1,24 @@
-import {Create, ImageField, ImageInput, required, SimpleForm, TextInput} from "react-admin"
+import { ThemeProvider } from "@mui/material"
+import {Create, ImageField, ImageInput, required, SelectField, SimpleForm, TextInput} from "react-admin"
+import BaseTheme from "../../constants/themes/BaseTheme"
+import { RichTextInput } from "ra-input-rich-text"
+import { SKIN_COLOR } from "../../constants/genetics/humans"
+
 
 const GeneticCreate = () => (
-  <Create>
+  <ThemeProvider theme={BaseTheme}>
+  <Create title={"Thêm mới mã di truyền"}>
     <SimpleForm>
         <TextInput source="name" validate={[required()]} />
-        <TextInput source="code" validate={[required()]} />
+        <TextInput source="type" validate={[required()]} />
         <TextInput source="status" validate={[required()]} />
-        <TextInput source="description" validate={[required()]} />
+        <RichTextInput source="description" validate={[required()]} />
+        <TextInput source="positive" validate={[required()]} />
+        <TextInput source="negative" validate={[required()]} />
+
     </SimpleForm>
   </Create>
+  </ThemeProvider>
 )
 
 export default GeneticCreate
